46e5cfe7227fd3295b1c6ac99fa67f32dcac910e
Shu-Zhao <zoe5120tw@gmail.com>
2017-03-17 23:23:07 +0800

    20170317

--- eSaleX/Controllers/OrderController.cs
+++ eSaleX/Controllers/OrderController.cs
@@ -11,11 +11,32 @@ namespace eSaleX.Controllers
         // GET: Order
         public ActionResult Index()
         {
-            Models.OrderService orderservice = new Models.OrderService;
+            Models.OrderService orderservice = new Models.OrderService();
             var order = orderservice.GetOrderById("111");
             ViewBag.CustId = order.CustId;
             ViewBag.CustName = order.CustName;
             return View();
+        }
+
+
+        public ActionResult Index2(string id)
+        {
+            ViewBag.id = id;
+            return View();
+        }
+        
+
+
+        public ActionResult InsertOrder()
+        {
+            return View();
+        }
+
+        [HttpPost()]
+
+        public ActionResult InserOrder(Models.Order order)
+        {
+            return View("Index");
         }
     }
 }
